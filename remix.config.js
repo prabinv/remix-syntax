2f4ac2b28c5973e9aabf4cdf3d728a2507df715a
Prabin <prabin.varma@volusion.com>
2022-04-13 20:50:06 -0500

    fix remix config

--- remix.config.js
+++ remix.config.js
@@ -5,7 +5,8 @@ module.exports = {
   ignoredRouteFiles: [".*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
-  serverBuildPath: "api/_build",
+  serverBuildPath: "api/_build/index.js",
+  serverBuildTarget: "node-cjs",
   // publicPath: "/build/",
   ignoredRouteFiles: [".*"],
 };
